0651e6e68d98e5370bc52d1039c8114fd1f00e8a
Rahul <rahulpratik1212@gmail.com>
2025-02-19 22:50:37 +0530

    feat: change in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { ThemeProvider } from '@mui/material';
 import { CssBaseline } from '@mui/material';
 import theme from './theme/theme';
-// import { useSelector } from 'react-redux';
 import { useAppSelector } from './hooks/useStore';
 import Router from './routes/Router';
 import Navbar from './ui/Navbar';
-// import data from './utils/users.json';
 
 function App() {
   const count = useAppSelector((state) => state.user);
