e03d2022186c991c0fb29fb9b952655359a41ebc
mr-developer01 <rahulpratik1212@gmail.com>
2025-02-21 04:31:34 -0800

    feat: add constant data for reference

---
+++ src/utils/constants.ts
@@ -0,0 +1,95 @@
+// import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
+
+// export const themeOptions: ThemeOptions = {
+
+//   palette: {
+
+//     type: 'light',
+
+//     primary: {
+
+//       main: '#1e3a5f',
+
+//     },
+
+//     secondary: {
+
+//       main: '#6fa3c7',
+
+//     },
+
+//     background: {
+
+//       default: '#f8f7f4',
+
+//     },
+
+//     text: {
+
+//       primary: '#222222',
+
+//       secondary: '#6FA3C7',
+
+//     },
+
+//   },
+
+//   typography: {
+
+//     h1: {
+
+//       fontFamily: 'Anton',
+
+//     },
+
+//     h2: {
+
+//       fontFamily: 'Anton',
+
+//     },
+
+//     h3: {
+
+//       fontFamily: 'Anton',
+
+//     },
+
+//     h6: {
+
+//       fontFamily: 'Anton',
+
+//     },
+
+//     h5: {
+
+//       fontFamily: 'Anton',
+
+//     },
+
+//     h4: {
+
+//       fontFamily: 'Anton',
+
+//     },
+
+//     body1: {
+
+//       fontFamily: 'Source Sans Pro',
+
+//     },
+
+//     body2: {
+
+//       fontFamily: 'Source Sans Pro',
+
+//     },
+
+//     button: {
+
+//       fontFamily: 'Source Sans Pro',
+
+//     },
+
+//   },
+
+// };
